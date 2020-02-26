e293ee473d04bb76db39090146ac25bfd9ec7aa3
caiwenduo <cawenduo@163.com>
2020-02-26 10:16:23 +0800

    修改配置文件

--- config/config.default.js
+++ config/config.default.js
@@ -11,6 +11,13 @@ module.exports = appInfo => {
    * @type {Egg.EggAppConfig}
    **/
   const config = exports = {};
+  config.cluster = {
+    listen: {
+      path: '',
+      port: 7001,
+      hostname: '0.0.0.0',
+    },
+  };
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1582648622306_9145';
